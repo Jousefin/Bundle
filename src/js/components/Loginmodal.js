2e3d98a3709f087ea4ebf6794ca40b2902cecb70
Jousefin <jou578@gmail.com>
2022-12-01 07:00:20 -0600

    login modal

--- src/js/components/Loginmodal.js
+++ src/js/components/Loginmodal.js
@@ -2,7 +2,7 @@ export function login (){
 	const d = document,
 	open = d.getElementById("btnLogin"), 
 	close = d.getElementById("closeLogin"),
-	modal = d.getElementById("login")
+	modal = d.getElementById("login-container")
 	open.addEventListener("click", ()=>{
 		modal.classList.add("active");
 	})
